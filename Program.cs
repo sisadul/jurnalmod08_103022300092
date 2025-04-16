bb674408d112a45c26d2d3a12b109ccd9941f33c
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-16 15:45:31 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace jurnal08_103022300092
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
